c44059e81d012a86cc8247d0cc94d0c2491fb84e
marso62 <junior46t@gmail.com>
2021-02-18 01:33:35 +0200

    Updates

---
+++ precache-manifest.dbead47145fbabbb59766f80599e30cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874c475a64e9888761fbf2a27d86d7df",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "475578b88951eecbfd6e",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.b8a9bdb6.chunk.css"
+  },
+  {
+    "revision": "ac39958779d1de993025",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.ba03d438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.ba03d438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "475578b88951eecbfd6e",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.72b932d8.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
